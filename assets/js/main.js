9bcfd7410756072ec52d496f6515b0da58fbb086
Francesca-Barcella <pollon.fra@gmail>
2022-10-19 14:02:14 +0200

    JSS - analisi in italiano

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,21 @@ Dividete in piccoli problemi la consegna.
 Individuate gli elementi di cui avete bisogno per realizzare il programma.
 ***************************************************/
 
-alert('ciao, funziona!');
+//alert('ciao, funziona!');
+
+// 1° Visualizzare in pagina 5 numeri casuali 
+    // - creare elemento in dom
+    // - selezionare elemento della dom in js
+    // - generare 5 numeri casuali math random
+    // - stampare i numeri nel dom
+// 2° Da lì parte un timer di 30 secondi.
+    // - creare elemento in dom per il timer
+    // - selezionare elemento della dom in js
+    // - setInterval per creare il timer di 30 secondi
+    // - stampare il timer nel dom
+// 3° Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt()
+    // - far sparire i numeri ... come?
+    // - chiedere all'utente quali numeri si ricorda UNO ALLA VOLTA!!! 
+    //   (prompt + ciclo, già visto in un esercizio, cercare)
+// 3° il software dice quanti e quali dei numeri da indovinare sono stati individuati
+    // - e qui mi vien da ridere... 
